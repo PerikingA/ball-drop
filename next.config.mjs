f47277248c58d116e344f54e2297429cc82be4ac
Periking <112653435+PerikingA@users.noreply.github.com>
2024-07-24 20:00:10 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
-  };/** @type {import('next').NextConfig} */
-const nextConfig = {
   output: 'export',
   basePath: '/ball-drop',
   assetPrefix: '/ball-drop',
