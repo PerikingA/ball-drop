0a19cba5d41a17c87211d16f044a31d6741a485c
Periking <112653435+PerikingA@users.noreply.github.com>
2024-07-24 20:04:12 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/ball-drop',
-  assetPrefix: '/ball-drop',
+  basePath: '/ball-drop', // Replace with your repository name
+  assetPrefix: '/ball-drop', // Replace with your repository name
 };
 
 export default nextConfig;
-
-  
-  export default nextConfig;
-  
